4a0f1ad2a8e89f666af743a2cc82a842e5e54f5c
TiagoSpada <tiago.spada.ts@gmail.com>
2024-06-23 21:11:59 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generatore di password</title>
+</head>
+<body>
+    <header>
+        <h1>GENERATORE DI PASSWORD</h1>
+    </header>
+    <main>
+        
+    </main>
+</body>
+</html>
